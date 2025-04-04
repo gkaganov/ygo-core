777f32ea2d406ece1874ce4df5d69c3ee995f57c
greg <a@a.a>
2025-04-04 11:31:36 +0200

    turn_player is now a method

--- src/ygo_core/core.rs
+++ src/ygo_core/core.rs
@@ -8,22 +8,12 @@ pub struct Core {
 impl Core {
     pub fn new(decks: [Vec<Card>; 2]) -> Self {
         Core {
-            state: State {
-                players: decks.map(|deck| Player::new(deck)),
-                turn_player: 0,
-            },
+            state: State::new(decks.map(|deck| Player::new(deck))),
         }
     }
 
     pub fn activate_card(&self, position_in_hand: usize) -> Core {
-        let card = self
-            .state
-            .players
-            .get(self.state.turn_player)
-            .unwrap()
-            .hand
-            .get(position_in_hand)
-            .unwrap();
+        let card = self.state.turn_player().hand.get(position_in_hand).unwrap();
         let state = (card.effect)(self.state.clone());
         Core { state }
     }
--- src/ygo_core/private/state.rs
+++ src/ygo_core/private/state.rs
@@ -2,19 +2,32 @@ use crate::ygo_core::private::types::Player;
 
 #[derive(Clone, Debug)]
 pub struct State {
-    pub turn_player: usize,
     pub players: [Player; 2],
+    turn_player_index: usize,
 }
 impl State {
+    pub fn new(players: [Player; 2]) -> Self {
+        State {
+            players,
+            turn_player_index: 0,
+        }
+    }
+
+    pub fn turn_player(&self) -> &Player {
+        &self.players[self.turn_player_index]
+    }
+
     pub fn draw_cards(&self, amount: usize) -> State {
         let mut players = self.players.clone();
-        let (hand, deck_after_draw) = players[self.turn_player].deck.split_at(amount);
-        players[self.turn_player].hand.append(&mut hand.to_vec());
-        players[self.turn_player].deck = deck_after_draw.to_vec();
+        let (hand, deck_after_draw) = players[self.turn_player_index].deck.split_at(amount);
+        players[self.turn_player_index]
+            .hand
+            .append(&mut hand.to_vec());
+        players[self.turn_player_index].deck = deck_after_draw.to_vec();
 
         State {
             players,
-            turn_player: self.turn_player,
+            turn_player_index: self.turn_player_index,
         }
     }
 }
